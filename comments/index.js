429e710847e7a85ee32bab44df254b14fc7ac31e
Steven Craig <stevencraig321@gmail.com>
2023-09-27 18:41:45 +0100

    feat: basic moderation service

--- comments/index.js
+++ comments/index.js
@@ -20,14 +20,15 @@ app.post('/posts/:id/comments', (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   }).catch((err) => {
     console.log(err.message);
