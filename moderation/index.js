81b297b50b96327cf7eb10e3959d5dce41011b8a
Steven Craig <stevencraig321@gmail.com>
2023-09-30 09:04:16 +0100

    feat: comment moderated

--- moderation/index.js
+++ moderation/index.js
@@ -6,7 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
-    
+    const {type, data} = req.data
+
+    if (type === "CommentCreated") {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+    }
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommnentCreated',
+        data: {
+            id: data.id,
+            postId: data.postId,
+            content: data.content,
+            status
+        }
+    })
+    res.send({})
 })
 
 app.listen(4003, () => {
